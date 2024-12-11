adcb1c4b299d70c8f523e26ca478575284d01226
William Cook <wjjcook11@gmail.com>
2024-12-10 20:57:23 -0500

    Create Character Select Screen class

---
+++ ui/character_select_screen.cpp
@@ -0,0 +1,109 @@
+#include "character_select_screen.hpp"
+
+CharacterSelectScreen::CharacterSelectScreen(SDL_Renderer* renderer, float scaleX, float scaleY, int localPlayer) : UI(renderer, scaleX, scaleY) {
+    this->localPlayer = localPlayer;
+    initializeElements();
+}
+
+CharacterSelectScreen::~CharacterSelectScreen() {}
+
+void CharacterSelectScreen::initializeElements() {
+    announcerText = new Text(renderer, "Terminal.ttf", 24, scaleX, scaleY);
+    announcerText->setText(" ", colorMap["white"]);
+
+    tempText = new Text(renderer, "Terminal.ttf", 48, scaleX, scaleY);
+    tempText->setText("NEW UNITS COMING SOON...", colorMap["white"]);
+
+    startButton = new Button(renderer, "Terminal.ttf", 24, "Ready!", colorMap["white"], colorMap["green"], 150, 60, scaleX, scaleY);
+    startButton->setOutline(true, colorMap["black"]);
+
+    Text* player1HeaderText = new Text(renderer, "Terminal.ttf", 24, scaleX, scaleY);
+    player1HeaderText->setText("Player 1 Units:", colorMap["light blue"]);
+
+    Text* player2HeaderText = new Text(renderer, "Terminal.ttf", 24, scaleX, scaleY);
+    player2HeaderText->setText("Player 2 Units:", colorMap["light red"]);
+
+    player1SelectText.push_back(player1HeaderText);
+    player2SelectText.push_back(player2HeaderText);
+
+    Button* blademasterButton = new Button(renderer, "Terminal.ttf", 16, "The Blademaster", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    blademasterButton->setOutline(true, colorMap["black"]);
+
+    Button* cavemanButton = new Button(renderer, "Terminal.ttf", 16, "The Caveman", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    cavemanButton->setOutline(true, colorMap["black"]);
+
+    Button* duelistButton = new Button(renderer, "Terminal.ttf", 16, "The Duelist", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    duelistButton->setOutline(true, colorMap["black"]);
+
+    Button* fighterButton = new Button(renderer, "Terminal.ttf", 16, "The Fighter", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    fighterButton->setOutline(true, colorMap["black"]);
+
+    Button* medicButton = new Button(renderer, "Terminal.ttf", 16, "The Medic", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    medicButton->setOutline(true, colorMap["black"]);
+
+    Button* raidBossButton = new Button(renderer, "Terminal.ttf", 16, "The Raid Boss", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    raidBossButton->setOutline(true, colorMap["black"]);
+
+    Button* tankButton = new Button(renderer, "Terminal.ttf", 16, "The Tank", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    tankButton->setOutline(true, colorMap["black"]);
+
+    Button* villageIdiotButton = new Button(renderer, "Terminal.ttf", 16, "The Village Idiot", colorMap["white"], colorMap["grey"], 150, 60, scaleX, scaleY);
+    villageIdiotButton->setOutline(true, colorMap["black"]);
+    
+    unitButtons.push_back(blademasterButton);
+    unitButtons.push_back(cavemanButton);
+    unitButtons.push_back(duelistButton);
+    unitButtons.push_back(fighterButton);
+    unitButtons.push_back(medicButton);
+    unitButtons.push_back(raidBossButton);
+    unitButtons.push_back(tankButton);
+    unitButtons.push_back(villageIdiotButton);
+}
+
+void CharacterSelectScreen::setText(TextType type, const std::string& newText) {
+    switch (type) {
+    case TextType::Announcer:
+        announcerText->setText(newText, colorMap["white"]);
+        break;
+    default:
+        std::cout << "Unsupported text type" << std::endl;
+        break;
+    }
+}
+
+Button* CharacterSelectScreen::getButton(ButtonType type) {
+    switch (type) {
+    case ButtonType::Start:
+        return startButton;
+    default:
+        std::cout << "Unsupported button type" << std::endl;
+        return nullptr;
+    }
+}
+
+std::vector<Button*> CharacterSelectScreen::getUnitButtons() {
+    return unitButtons;
+}
+
+void CharacterSelectScreen::render() {
+    announcerText->render(25, 450);
+    tempText->render(50, 300);
+    startButton->render(725, 430);
+    for (unsigned int i = 0; i < unitButtons.size(); i++) {
+        if (i < 4) {
+            unitButtons[i]->render((i*175)+25, 50);
+        } else {
+            unitButtons[i]->render(((i-4)*175)+25, 150);
+        }
+        
+    }
+    if (localPlayer == 1) {
+        for (unsigned int i = 0; i < player1SelectText.size(); i++) {
+            player1SelectText[i]->render(725, ((i)*35)+50);
+        }
+    } else {
+        for (unsigned int i = 0; i < player2SelectText.size(); i++) {
+            player2SelectText[i]->render(725, ((i)*35)+50);
+        }
+    }
+}
---
+++ ui/character_select_screen.hpp
@@ -0,0 +1,37 @@
+#ifndef CHARACTER_SELECT_SCREEN_H
+#define CHARACTER_SELECT_SCREEN_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "ui.hpp"
+#include "../text.hpp"
+#include "../button.hpp"
+
+class CharacterSelectScreen : public UI {
+    public:
+        CharacterSelectScreen(SDL_Renderer* renderer, float scaleX, float scaleY, int localPlayer);
+        ~CharacterSelectScreen() override;
+
+        void initializeElements() override;
+
+        void setText(TextType type, const std::string& newText) override;
+        Button* getButton(ButtonType type) override;
+        std::vector<Button*> getUnitButtons();
+        void render() override;
+
+    private:
+        int localPlayer;
+        Text* announcerText;
+        Text* tempText;
+        std::vector<Text*> player1SelectText;
+        std::vector<Text*> player2SelectText;
+
+        Button* startButton;
+        std::vector<Button*> unitButtons;
+};
+
+#endif
